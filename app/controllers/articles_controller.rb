90f29c8cec06cc2b40788cbe159445c80c29c267
Gaurank Sharma <gauranksharma@gaurank.local>
2017-05-17 14:23:06 +0530

    17th may work of blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "gaurank", password: "root", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -8,13 +11,38 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
@@ -23,3 +51,4 @@ class ArticlesController < ApplicationController
   end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :articles do
+    resources :comments
+  end
+
 end
